c0c61c90a3843dec2be1470f2c8cf535c0c863a3
Your Name <you@example.com>
2020-07-03 03:51:24 +0000

    a

--- install-pcf/templates/create-vm.sh
+++ install-pcf/templates/create-vm.sh
@@ -2,7 +2,7 @@
 # code_snippet create-vm-script start bash
 
 cat /var/version && echo ""
-set -eux
+set -eu
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
