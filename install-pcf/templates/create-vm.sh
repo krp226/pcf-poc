2f4fd2520416217473f93a5f4cd27b68dff81116
Your Name <you@example.com>
2020-07-03 03:43:18 +0000

    a

--- install-pcf/templates/create-vm.sh
+++ install-pcf/templates/create-vm.sh
@@ -1,5 +1,52 @@
 #!/usr/bin/env bash
-set -eu
-echo "Hello"
+# code_snippet create-vm-script start bash
+
+cat /var/version && echo ""
+set -eux
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+
+vars_files_args=("")
+for vf in ${VARS_FILES}
+do
+  vars_files_args+=("--vars-file ${vf}")
+done
+
+# '$timestamp' must be a literal, because envsubst uses it as a filter
+# this allows us to avoid accidentally interpolating anything else.
+# shellcheck disable=SC2016
+INPUT_STATE_FILE="$(echo "$STATE_FILE" | env timestamp='*' envsubst '$timestamp')"
+
+# '$timestamp' must be a literal, because envsubst uses it as a filter
+# this allows us to avoid accidentally interpolating anything else.
+# shellcheck disable=SC2016
+OUTPUT_FILE_NAME="$(echo "$STATE_FILE" | env timestamp="$(date '+%Y%m%d.%-H%M.%S+%Z')" envsubst '$timestamp')"
+GENERATED_STATE_FILE_NAME="$(basename "$OUTPUT_FILE_NAME")"
+
+export IMAGE_FILE
+IMAGE_FILE="$(find image/*.{yml,ova,raw} 2>/dev/null | head -n1)"
+
+if [ -z "$IMAGE_FILE" ]; then
+  echo "No image file found in image input."
+  echo "Contents of image input:"
+  ls -al image
+  exit 1
+fi
+
+# ${vars_files_args[@] needs to be globbed to split properly (SC2068)
+# INPUT_STATE_FILE need to be globbed (SC2086)
+# shellcheck disable=SC2068,SC2086
+p-automator create-vm \
+--config "config/${OPSMAN_CONFIG_FILE}" \
+--image-file "${IMAGE_FILE}"  \
+--state-file state/${INPUT_STATE_FILE} \
+${vars_files_args[@]}
+
+# INPUT_STATE_FILE need to be globbed (SC2086)
+# shellcheck disable=SC2086
+cp state/${INPUT_STATE_FILE} "generated-state/${GENERATED_STATE_FILE_NAME}"
+
 # code_snippet create-vm-script end
 
