4370a63a8a6384170807fedebbcfb4f3323b98e6
kartikeyavadlapatla <vkartheek.ky1311@gmail.com>
2022-01-11 10:49:14 -0500

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,15 +1,27 @@
-#!/bin/bash -eu
-chmod +x ./jq/jq-linux64
-cp jq/jq-linux64 /usr/local/bin/jq
+#!/bin/bash
 
-chmod +x ./om-cli/om-linux
-cp om-cli/om-linux /usr/local/bin/om
-chmod +x ./pivnet-cli/pivnet-linux-*
-cp pivnet-cli/pivnet-linux-* /usr/local/bin/pivnet
-echo $ZSCALER_CERT |  sed 's/----- /-----\n/g' | sed 's/ -----/\n-----/g' | sed '2s/ /\n/g' > zscaler.crt
-mv zscaler.crt /usr/local/share/ca-certificates/
-update-ca-certificates
-#awk -v cmd='openssl x509 -noout -subject' '/BEGIN/{close(cmd)};{print | cmd}' < /etc/ssl/certs/ca-certificates.crt
+apt-get update
+apt-get install gnupg2 wget vim -y
+
+wget https://github.com/pivotal-cf/pivnet-cli/releases/download/v2.0.2/pivnet-linux-amd64-2.0.2
+mv pivnet-linux-amd64-2.0.2 /usr/local/bin/pivnet
+chmod +x /usr/local/bin/pivnet
+
+#wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | apt-key add -
+#echo "deb http://apt.starkandwayne.com stable main" | tee /etc/apt/sources.list.d/starkandwayne.list
+
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+
+apt-get update
+#apt-get install om jq cf-cli -y
+
+apt-get install cf-cli -y
+
+cf install-plugin -r CF-Community "report-buildpacks" -f
+cf login -a ${Endpoint} -u ${USERNAME} -p ${PASSWORD} -o ${ORG} -s ${SPACE} --skip-ssl-validation
+cf report-buildpacks
+
 pivnet login --api-token=${PIVNET_API_TOKEN}
 
 function check_pivnet_version(){
@@ -20,6 +32,14 @@ function check_pivnet_version(){
   echo $PIV_VERSION
 
 }
+
+function check_pivnet_latest_version(){
+
+  PIV_LATEST_VERSION=$(pivnet releases -p $1 | sed -n '4p' | cut -d'|' -f3,4)
+  echo $PIV_LATEST_VERSION
+
+}
+
 
 products=$(om -t ${OPSMAN_URL} -u ${OPSMAN_USERNAME} -p ${OPSMAN_PASSWORD} -k curl --path /api/v0/deployed/products | jq -r '.[]' | jq -r '.type,.product_version,"\\n"')
 
@@ -37,34 +57,61 @@ echo -e $products | while read line; do
   if [ ${PRODUCT} == cf ];then
    PRODUCT=elastic-runtime
    MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}"
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}" >> generated-versions/current_versions.txt
+   echo "CURRENT VERSION DETAILS: Product: ${PRODUCT}, Major Version is: ${MAJOR_VERSION}"
    PIVNET_VERSION=$(check_pivnet_version $PRODUCT $MAJOR_VERSION)
    PIVNET_VERSION=$( echo $PIVNET_VERSION | cut -d'|' -f1)
-   echo "Pivnet latest version is : $PIVNET_VERSION"
-   echo "Pivnet latest version is : $PIVNET_VERSION" >> generated-versions/current_versions.txt
+   PIVNET_LATEST_VERSION=$(check_pivnet_latest_version $PRODUCT)
+   PIVNET_LATEST_VERSION=$( echo $PIVNET_LATEST_VERSION | cut -d'|' -f1)
+   echo "Current Version is:              $VERSION"
+   echo "Pivnet latest minor version is:  $PIVNET_VERSION"
+   echo "Pivnet latest major version is:  $PIVNET_LATEST_VERSION"
   elif [ ${PRODUCT} == p-bosh ];then
    PRODUCT=ops-manager
    MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
    CURRENT_VERSION=$(pivnet releases -p $PRODUCT | grep $VERSION -B1 | cut -d'|' -f3)
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Real Version is: $CURRENT_VERSION , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}"
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Real Version is: $CURRENT_VERSION , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}" >> generated-versions/current_versions.txt
+   echo "CURRENT VERSION DETAILS: Product: $PRODUCT, Major Version is: $MAJOR_VERSION"
    #Speical processing
    PIVNET_VERSION_OUT=$(check_pivnet_version $PRODUCT $MAJOR_VERSION)
    PIVNET_VERSION=$( echo $PIVNET_VERSION_OUT | cut -d'|' -f1)
-   PIVNET_SPECIAL_VERSION=$( echo $PIVNET_VERSION_OUT | cut -d'|' -f3)
-   echo "Pivnet latest version is : $PIVNET_VERSION" 
-   echo "Pivnet special version is : $PIVNET_SPECIAL_VERSION"
-   echo "Pivnet latest version is : $PIVNET_VERSION" >> generated-versions/current_versions.txt 
-   echo "Pivnet special version is : $PIVNET_SPECIAL_VERSION" >> generated-versions/current_versions.txt 
-  else
-    MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}"
-   echo "CURRENT VERSION DETAILS:  Product : ${PRODUCT} , Version is : ${VERSION} , Major Version is : ${MAJOR_VERSION}" >> generated-versions/current_versions.txt
+   PIVNET_SPECIAL_VERSION=$( echo $PIVNET_VERSION_OUT | cut -d'|' -f3,3)
+   PIVNET_LATEST_VERSION=$(check_pivnet_latest_version $PRODUCT)
+   PIVNET_LATEST_VERSION=$( echo $PIVNET_LATEST_VERSION | cut -d'|' -f1)
+   echo "Current Version is:              $VERSION"
+   echo "Pivnet latest minor version is:  $PIVNET_VERSION"
+   #echo "Pivnet special version is:       $PIVNET_SPECIAL_VERSION"
+   echo "Pivnet latest major version is:  $PIVNET_LATEST_VERSION"
+  elif [ ${PRODUCT} == appMetrics ];then
+   PRODUCT=apm
+   MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
+   echo "CURRENT VERSION DETAILS: Product: ${PRODUCT}, Major Version is: ${MAJOR_VERSION}"
    PIVNET_VERSION=$(check_pivnet_version $PRODUCT $MAJOR_VERSION)
    PIVNET_VERSION=$( echo $PIVNET_VERSION | cut -d'|' -f1)
-   echo "Pivnet latest version is : $PIVNET_VERSION"
-   echo "Pivnet latest version is : $PIVNET_VERSION" >> generated-versions/current_versions.txt
+   PIVNET_LATEST_VERSION=$(check_pivnet_latest_version $PRODUCT)
+   PIVNET_LATEST_VERSION=$( echo $PIVNET_LATEST_VERSION | cut -d'|' -f1)
+   echo "Current Version is:              $VERSION"
+   echo "Pivnet latest minor version is:  $PIVNET_VERSION"
+   echo "Pivnet latest major version is:  $PIVNET_LATEST_VERSION"
+  elif [ ${PRODUCT} ==  metric-store ];then
+   PRODUCT=p-metric-store
+   MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
+   echo "CURRENT VERSION DETAILS: Product: ${PRODUCT}, Major Version is: ${MAJOR_VERSION}"
+   PIVNET_VERSION=$(check_pivnet_version $PRODUCT $MAJOR_VERSION)
+   PIVNET_VERSION=$( echo $PIVNET_VERSION | cut -d'|' -f1)
+   PIVNET_LATEST_VERSION=$(check_pivnet_latest_version $PRODUCT)
+   PIVNET_LATEST_VERSION=$( echo $PIVNET_LATEST_VERSION | cut -d'|' -f1)
+   echo "Current Version is:              $VERSION"
+   echo "Pivnet latest minor version is:  $PIVNET_VERSION"
+   echo "Pivnet latest major version is:  $PIVNET_LATEST_VERSION"
+  else
+   MAJOR_VERSION=$(echo $VERSION | grep -o '[0-9]\{1,2\}\.[0-9]\{1,2\}')
+   echo "CURRENT VERSION DETAILS: Product: ${PRODUCT}, Major Version is: ${MAJOR_VERSION}"
+   PIVNET_VERSION=$(check_pivnet_version $PRODUCT $MAJOR_VERSION)
+   PIVNET_VERSION=$( echo $PIVNET_VERSION | cut -d'|' -f1)
+   PIVNET_LATEST_VERSION=$(check_pivnet_latest_version $PRODUCT)
+   PIVNET_LATEST_VERSION=$( echo $PIVNET_LATEST_VERSION | cut -d'|' -f1)
+   echo "Current Version is:              $VERSION"
+   echo "Pivnet latest minor version is:  $PIVNET_VERSION"
+   echo "Pivnet latest major version is:  $PIVNET_LATEST_VERSION"
   fi
 echo ""
 done
